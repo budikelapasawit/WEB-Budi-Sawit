158c64e1ff5b72bed14c7fd3237016d4376b63f0
Kevin Arden Islamey <kevinarden.islamey@gmail.com>
2020-12-06 15:10:14 +0700

    Product view

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/users/{name?}',function($name=null){
+    return 'Hi ' . $name;
+});
+
+Route::get('/products/{id}', function($id){
+    return 'Product id is: ' . $id;
+});
+
+Route::match(['get','post'], '/students', function(Request $req){
+    return 'Requested method is '. $req->method();
+});
+
+Route::any('/posts', function(Request $req){
+    return 'Requested method is '. $req->method;
+});
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home/{name}',[HomeController::class,'index'])->name('home.index');
+
+Route::get('/product',function(){
+    return view('product');
 });
